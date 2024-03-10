921446d94eab3b8e74c679840e3bf2db4603b179
xu-kai-xu <xuk1111@foxmail.com>
2024-03-10 21:12:14 +0800

    打印二叉树

--- btree/main.c
+++ btree/main.c
@@ -20,6 +20,12 @@ struct node_st {
     struct node_st *l, *r;
 };
 
+void print_s(struct score_st *d)
+{
+    printf("%d %s %d %d\n", d->id, d->name, d->math, d->chinese);
+    // printf("%d\n", d->id);
+}
+
 /*
 根据id，插入节点。如果data的id小于等于当前节点，就挂到左子树，否则挂到右子树
 */
@@ -34,8 +40,8 @@ int insert(struct node_st **root, struct score_st *data)
         }
 
         node->data = *data;
-        node->l = NULL;
-        node->r = NULL;
+        node->l = NULL;  // 防止野指针
+        node->r = NULL;  // 防止野指针
 
         *root = node;
         return 0;
@@ -48,22 +54,93 @@ int insert(struct node_st **root, struct score_st *data)
     }
 }
 
-struct score_st find(struct node_st *root, int id)
+struct score_st *find(struct node_st *root, int id)
 {
+    if (root == NULL) {
+        return NULL;
+    }
     if (id == root->data.id) {
         return &root->data;
     }
 
-    if (id < root->data.id) {
-        find()
+    if (id <= root->data.id) {
+        return find(root->l, id);
+    }
+    if (id > root->data.id) {
+        return find(root->r, id);
     }
 }
+
+void draw_tree_node(struct node_st *root, int level)
+{
+    int i, j;
+    if (root == NULL) {
+        return;
+    }
+
+    // 画根节点
+    printf("%d\n", root->data.id);
+
+
+    // 画左子树
+    if (root->l != NULL) {
+        for (i = 0; i < level * 3; i++) {
+            printf(" ");
+        }
+        printf("|");
+        printf("--");
+        draw_tree_node(root->l, level + 1);
+    }
+
+    // 画右子树
+    if (root->r != NULL) {
+        for (i = 0; i < level * 3; i++) {
+            printf(" ");
+        }
+        printf("|");
+        printf("--");
+        draw_tree_node(root->r, level + 1);
+    }
+}
+
+/*
+可以采用类似于tree命令的方式画树
+*/
+void draw_tree(struct node_st *root)
+{
+    draw_tree_node(root, 0);
+}
+
+void draw_(struct node_st *root, int level)
+{
+    if (root == NULL) {
+        return;
+    }
+
+    // 先画右子树
+    draw_(root->r, level + 1);
+
+    // 再画当前节点
+    for (int i = 0; i < level; i++) {
+        printf("    ");
+    }
+    print_s(&root->data);
+
+    // 再画左子树
+    draw_(root->l, level + 1);
+}
+
+void draw(struct node_st *root)
+{
+    draw_(root, 0);
+}
+
 
 int main()
 {
     int arr[] = {1, 2, 3, 7, 6, 5, 9, 8, 4};
     struct node_st *tree = NULL;
-    struct score_st tmp;
+    struct score_st tmp, *datap;
 
     for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) {
         tmp.id = arr[i];
@@ -73,8 +150,18 @@ int main()
         insert(&tree, &tmp);
     }
 
-    int tmpid = 2;
-    find(tree, tmpid);
+    // 画树
+    // draw(tree);
+    draw_tree(tree);
 
+#if 0
+    int tmpid = 12;
+    datap = find(tree, tmpid);
+    if(datap == NULL) {
+        printf("can not find id: %d\n", tmpid);
+    } else {
+        print_s(datap);
+    }
+#endif
     exit(0);
 }
--- demo.c
+++ demo.c
@@ -1,2 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int main()
+{
+    int i = 1;
+    int a = (++i) + (++i);
+    printf("a = %d\n", a);
+    return 0;
+}
